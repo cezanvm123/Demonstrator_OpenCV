8d2135000b124e2b0a6eb49d098a61db240e0890
Cezan <cezanvonmeijenfeldt@gmail.com>
2021-01-06 15:48:47 +0100

    DFT working with masks

---
+++ Tests/TestProject_V1/TestProject_V1/DFT.hpp
@@ -0,0 +1,215 @@
+#pragma once
+// https://docs.opencv.org/3.4/d8/d01/tutorial_discrete_fourier_transform.html
+
+#define DEBUG true
+
+#include "Test.hpp"
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace std;
+using namespace cv;
+
+class DFT : public Test
+{
+
+public:
+
+
+	DFT(bool displayImg, string filter, string testName, string path) :
+		displayImg(displayImg), filterPath(filter), Test(testName, path, 1)
+	{ }
+
+
+
+
+
+	void runTest()
+	{
+
+		loadImage();
+		if(displayImg)
+			imshow ("original", image);
+
+		Mat dft; 
+		getDFT(image, dft);
+		if(DEBUG)
+			visualizeDFT(dft, "non centered");
+
+
+		recenterDFT(dft);
+		if(DEBUG)
+			visualizeDFT(dft, "Centered");
+
+
+		// apply filter
+		applyFilter(dft);
+		if (DEBUG)
+			visualizeDFT(dft, "After filter");
+
+
+		de_recenterDFT(dft);
+		if(DEBUG)
+			visualizeDFT(dft, "de-reCentered?");
+
+		Mat result; 
+		invertDFT(dft, result);
+		if(displayImg)
+			imshow("result", result);
+
+	}
+
+
+private:
+
+
+	void applyFilter(Mat& dft)
+	{
+		if (DEBUG)
+			imshow("filter", filter);
+
+
+		Mat splitList[2] = { Mat::zeros(dft.size(), CV_32F),Mat::zeros(dft.size(), CV_32F) };
+		split(dft, splitList);
+
+		Mat destList[2] = { Mat::zeros(dft.size(), CV_32F),Mat::zeros(dft.size(), CV_32F) };
+
+		splitList[0].copyTo(destList[0], filter);
+		splitList[1].copyTo(destList[1], filter);
+
+		merge(destList, 2, dft);
+		
+	}
+
+
+	void loadImage()
+	{
+		cv::Mat original = cv::imread(getPath(), cv::IMREAD_COLOR);
+		filter = cv::imread(filterPath, cv::IMREAD_GRAYSCALE);
+		timingStop();
+
+		// conversion to grayscale image
+		timingStart(MeasurementType::IMAGE_CONVERTION);
+		cvtColor(original, image, cv::COLOR_BGR2GRAY);
+	}
+
+
+	void getDFT(Mat& input, Mat& output)
+	{
+		Mat imageFloat;
+
+		//convert from 0 - 255 to 0 - 1 needed for dft function
+		input.convertTo(imageFloat, CV_32FC1, 1.0 / 255.0);
+
+		// create Mat object array the first value holds the real data and the second the img data
+		// the complex Mat object is filled with 0's 
+		Mat imageComplex[2] = { imageFloat, Mat::zeros(input.size(), CV_32F) };
+
+		// this Mat object will contain the real and the imaginary values. 
+		Mat combined;
+		merge(imageComplex, 2, combined);
+
+
+		
+		dft(combined, output, DFT_COMPLEX_OUTPUT);
+	}
+
+
+
+	void invertDFT(Mat& dftSource, Mat& img)
+	{
+
+		// flags
+		// - DFT_INVERSE:		This lets the dft function know that we want the inverse and thus passing a complex Mat
+		// - DFT_REAL_OUTPUT:	This lets the dft function know we only want real values(as we cannot display complex numbers).
+		// - DFT_SCALE			Makes sure the values are normalized
+		dft(dftSource, img, DFT_INVERSE | DFT_REAL_OUTPUT | DFT_SCALE);
+	}
+
+	// recenterDFT but in reverse
+	void de_recenterDFT(Mat& dftSource)
+	{
+		int centerX = dftSource.cols / 2;
+		int centerY = dftSource.rows / 2;
+
+
+		Mat c1(dftSource, Rect(0, 0, centerX, centerY));
+		Mat c2(dftSource, Rect(centerX, 0, centerX, centerY));
+
+		Mat c3(dftSource, Rect(0, centerY, centerX, centerY));
+		Mat c4(dftSource, Rect(centerX, centerY, centerX, centerY));
+
+
+		Mat temp;
+		c4.copyTo(temp);
+		c1.copyTo(c4);  
+		temp.copyTo(c1); 
+
+		c3.copyTo(temp);
+		c2.copyTo(c3);  
+		temp.copyTo(c2); 
+	}
+
+
+	// receneter the dft sow the low freq is in the middle and high freq at the corners
+	void recenterDFT(Mat& dftSource)
+	{
+		int centerX = dftSource.cols / 2;
+		int centerY = dftSource.rows / 2;
+
+		
+		Mat c1(dftSource, Rect(0, 0, centerX, centerY));
+		Mat c2(dftSource, Rect(centerX, 0, centerX, centerY));
+
+		Mat c3(dftSource, Rect(0, centerY, centerX, centerY));
+		Mat c4(dftSource, Rect(centerX, centerY, centerX, centerY));
+
+
+		Mat temp; 
+		c1.copyTo(temp);
+		c4.copyTo(c1);  //corner4 is now overwritten with data from corner1
+		temp.copyTo(c4); // corner1's data stored in the temp var is now written to corner4
+
+		c2.copyTo(temp);
+		c3.copyTo(c2);  //corner4 is now overwritten with data from corner1
+		temp.copyTo(c3); // corner1's data stored in the temp var is now written to corner4
+
+	}
+
+	// this function is used for visualizing only the dft cannot be used for image processing after the normilization.
+	// reference is not used here to make sure the original is not altered.
+	void visualizeDFT(Mat dftSource, string name )
+	{
+		Mat splitList[2] = { Mat::zeros(dftSource.size(), CV_32F),Mat::zeros(dftSource.size(), CV_32F) };
+
+		split(dftSource, splitList);
+
+		// will hold the dft magnitudes for better visualization. 
+		Mat dftMag;
+
+		magnitude(splitList[0], splitList[1], dftMag);
+
+		// adds a 1 to all the magnitudes to make sure the following part won't fail. 
+		dftMag += Scalar::all(1);
+
+		// cannot take the log of 0
+		log(dftMag, dftMag);
+
+		// further normilazing the values to be between 0 - 1
+		normalize(dftMag, dftMag, 0, 1, NORM_MINMAX);
+
+		imshow(name, dftMag);
+		waitKey(1);
+	}
+
+
+	string filterPath;
+
+	bool displayImg;
+
+	cv::Mat image;
+	cv::Mat filter;
+
+
+};
--- Tests/TestProject_V1/TestProject_V1/TestProject_V1.cpp
+++ Tests/TestProject_V1/TestProject_V1/TestProject_V1.cpp
@@ -10,7 +10,7 @@
 
 #include "Test.hpp"
 
-
+#include "DFT.hpp"
 #include "Kernel_Image.hpp"
 #include "Kernel_Video.hpp"
 #include "SeparableKernel_Image.hpp"
@@ -30,119 +30,235 @@ int main()
 
 	String imgPath = "D:\\School\\Signals\\Demonstrator_OpenCV\\Tests\\TestProject_V1\\TestProject_V1\\TestImages\\openCV_240.png";
 	String vidPath = "D:\\School\\Signals\\Demonstrator_OpenCV\\Tests\\TestProject_V1\\TestProject_V1\\TestImages\\openCV.mp4";
+	
+	string dftFilterPath = "D:\\School\\Signals\\Demonstrator_OpenCV\\Tests\\TestProject_V1\\TestProject_V1\\TestImages\\Masks\\Mask1_lowpass.png";
+	
 	{
-
-		float data[9] = { -1,  -1,  -1,
-						  -1,   8,  -1,
-						  -1,  -1,  -1 };
-		Test* kernel = new Kernel_Image(3,
-			data,
-			1,
-			true,
-			"kernel, line test, image",
-			roadImg);
-		kernel->runTest();
-
+		Test* dftTest = new DFT(true, dftFilterPath, "DFT test", imgPath);
+		dftTest->runTest();
 	}
+	
+//	
+//	/*
+//	- Gaussian Blur test
+//	- kernel and separable kernel
+//	- video
+//
+//*/
+//	{
+//		float data[9] = { -1,  -1,  -1,
+//						  -1,   8,  -1,
+//						  -1,  -1,  -1 };
+//		Test* kernel = new Kernel_Video(3,
+//			data,
+//			1,
+//			false,
+//			1100,
+//			"kernel, Gaussian Blur, video",
+//			vidPath);
+//		kernel->runTest();
+//
+//
+//		float h_vector[3] = { -1,0,1 }; // horizontal
+//		float v_vector[3] = { 1,2,1 }; // vertical 
+//		Test* sepKernel = new SeparableKernel_Video(3,
+//			h_vector,
+//			v_vector,
+//			1,
+//			false,
+//			1100,
+//			"separable, Gaussian Blur, video",
+//			vidPath);
+//		sepKernel->runTest();
+//
+//
+//	}
+//	
+//	
+//	/*
+//		- Gaussian Blur test 
+//		- kernel and separable kernel 
+//		- video
+//
+//	*/
+//	{
+//		float data[9] = { 0.077847,  0.123317,  0.077847,
+//						  0.123317,  0.195346,  0.123317,
+//						  0.077847,  0.123317,  0.077847 };
+//		Test* kernel = new Kernel_Video(3,
+//			data,
+//			1,
+//			false,
+//			1100,
+//			"kernel, Gaussian Blur, video",
+//			vidPath);
+//		kernel->runTest();
+//
+//
+//		float h_vector[3] = { 0.27901, 0.44198, 0.27901 }; // horizontal
+//		float v_vector[3] = { 0.27901, 0.44198, 0.27901 }; // vertical 
+//		Test* sepKernel = new SeparableKernel_Video(3,
+//			h_vector,
+//			v_vector,
+//			1,
+//			false,
+//			1100,
+//			"separable, Gaussian Blur, video",
+//			vidPath);
+//		sepKernel->runTest();
+//
+//
+//	}
+	
+	/*
+	- Sharpening test
+	- kernel and separable kernel
+	- video
 
-	{
-
-		float h_vector[3] = { -1,0,1 }; // horizontal
-		float v_vector[3] = { 1,2,1 }; // vertical 
-		Test* kernel = new SeparableKernel_Image(3,
-			h_vector,
-			v_vector,
-			1,
-			true,
-			"separable, line test, image",
-			roadImg);
-		kernel->runTest();
-
-	}
-
-	{
-
-		float data[9] = { 0.077847,  0.123317,  0.077847,
-						  0.123317,  0.195346,  0.123317,
-			              0.077847,  0.123317,  0.077847 };
-
-		Test* kernel = new Kernel_Image(3,
-			data,
-			1,
-			false,
-			"kernel, Gaussian, image",
-			imgPath);
-		kernel->runTest();
-
-	}
-
-	{
-
-		float h_vector[3] = { 0.27901, 0.44198, 0.27901 }; // horizontal
-		float v_vector[3] = { 0.27901, 0.44198, 0.27901 }; // vertical 
-		Test* kernel = new SeparableKernel_Image(3,
-			h_vector,
-			v_vector,
-			1,
-			false,
-			"separable, Gaussian, image",
-			imgPath);
-		kernel->runTest();
-
-	}
-
-	{
-		float h_vector[3] = { -1,0,1 }; // horizontal
-		float v_vector[3] = { 1,2,1 }; // vertical 
-		Test* kernel = new SeparableKernel_Video(3,
-			h_vector,
-			v_vector,
-			1,
-			false,
-			1100,
-			"separable, line test, video",
-			vidPath);
-		kernel->runTest();
-	}
-
+*/
 	//{
-	//	float h_vector[3] = { 0.27901, 0.44198, 0.27901 }; // horizontal
-	//	float v_vector[3] = { 0.27901, 0.44198, 0.27901 }; // vertical 
+	//	float data[9] = { 0,  -1,  0,
+	//					  -1,  5,  -1,
+	//					  0,  -1,  0 };
+	//	Test* kernel = new Kernel_Video(3,
+	//		data,
+	//		1,
+	//		true,
+	//		1100,
+	//		"kernel, Sharpening test, video",
+	//		vidPath);
+	//	kernel->runTest();
 
-	//	Test* kernel = new SeparableKernel_Video(3,
+
+	//	float h_vector[3] = { -1, 1, -1}; // horizontal
+	//	float v_vector[3] = { 1, 9, 1 }; // vertical 
+	//	Test* sepKernel = new SeparableKernel_Video(3,
 	//		h_vector,
 	//		v_vector,
 	//		1,
 	//		true,
-	//		871,
-	//		"separable, Gaussian, video",
+	//		1100,
+	//		"separable, Sharpening test, video",
+	//		vidPath);
+	//	sepKernel->runTest();
+
+
+	//}
+	//
+	
+	//{
+
+	//	float data[9] = { -1,  -1,  -1,
+	//					  -1,   8,  -1,
+	//					  -1,  -1,  -1 };
+	//	Test* kernel = new Kernel_Image(3,
+	//		data,
+	//		1,
+	//		true,
+	//		"kernel, line test, image",
+	//		roadImg);
+	//	kernel->runTest();
+
+	//}
+
+	//{
+
+	//	float h_vector[3] = { -1,0,1 }; // horizontal
+	//	float v_vector[3] = { 1,2,1 }; // vertical 
+	//	Test* kernel = new SeparableKernel_Image(3,
+	//		h_vector,
+	//		v_vector,
+	//		1,
+	//		true,
+	//		"separable, line test, image",
+	//		roadImg);
+	//	kernel->runTest();
+
+	//}
+
+	//{
+
+	//	float data[9] = { 0.077847,  0.123317,  0.077847,
+	//					  0.123317,  0.195346,  0.123317,
+	//		              0.077847,  0.123317,  0.077847 };
+
+	//	Test* kernel = new Kernel_Image(3,
+	//		data,
+	//		1,
+	//		false,
+	//		"kernel, Gaussian, image",
+	//		imgPath);
+	//	kernel->runTest();
+
+	//}
+
+	//{
+
+	//	float h_vector[3] = { 0.27901, 0.44198, 0.27901 }; // horizontal
+	//	float v_vector[3] = { 0.27901, 0.44198, 0.27901 }; // vertical 
+	//	Test* kernel = new SeparableKernel_Image(3,
+	//		h_vector,
+	//		v_vector,
+	//		1,
+	//		false,
+	//		"separable, Gaussian, image",
+	//		imgPath);
+	//	kernel->runTest();
+
+	//}
+
+	//{
+	//	float h_vector[3] = { -1,0,1 }; // horizontal
+	//	float v_vector[3] = { 1,2,1 }; // vertical 
+	//	Test* kernel = new SeparableKernel_Video(3,
+	//		h_vector,
+	//		v_vector,
+	//		1,
+	//		false,
+	//		1100,
+	//		"separable, line test, video",
+	//		vidPath);
+	//	kernel->runTest();
+	//}
+
+	////{
+	////	float h_vector[3] = { 0.27901, 0.44198, 0.27901 }; // horizontal
+	////	float v_vector[3] = { 0.27901, 0.44198, 0.27901 }; // vertical 
+
+	////	Test* kernel = new SeparableKernel_Video(3,
+	////		h_vector,
+	////		v_vector,
+	////		1,
+	////		true,
+	////		871,
+	////		"separable, Gaussian, video",
+	////		vidPath);
+	////	kernel->runTest();
+
+	////}
+ ////   
+
+
+	//{
+
+	//	float data[9] = { -1,  -1,  -1,
+	//					  -1,   8,  -1,
+	//					  -1,  -1,  -1 };
+	//	Test* kernel = new Kernel_Video(3,
+	//		data,
+	//		1,
+	//		true,
+	//		1100,
+	//		"kernel, line test, video",
 	//		vidPath);
 	//	kernel->runTest();
 
 	//}
- //   
-
-
-	{
-
-		float data[9] = { -1,  -1,  -1,
-						  -1,   8,  -1,
-						  -1,  -1,  -1 };
-		Test* kernel = new Kernel_Video(3,
-			data,
-			1,
-			true,
-			1100,
-			"kernel, line test, video",
-			vidPath);
-		kernel->runTest();
-
-	}
 
 
 
 
 
-  //  waitKey(0);
+    waitKey(0);
     return 0;
 }
